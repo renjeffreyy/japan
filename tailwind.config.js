abfb29c4ffd59593440637ac5d4a7f3f85ebb568
jeffrey ren <jeffreyren@jeffreys-MacBook-Pro.local>
2024-05-07 01:59:23 +0900

    added basic layout of page

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors:{
+      "sakura-pink": "#fcd1d7",
+    },
     extend: {},
   },
   plugins: [],
