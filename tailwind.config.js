0c0b1b1bab71638898709d7d54a96282bb645ba5
jeffrey ren <jeffreyren@jeffreys-MacBook-Pro.local>
2024-05-12 20:15:10 +0900

    added font file for headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    fontFamily:{
+      "serif":["Zen Tokyo Zoo"]
+    },
     colors:{
       "sakura-pink": "#fcd1d7",
     },
