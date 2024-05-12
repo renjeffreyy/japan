8a080ab297c0099da6c01b10eed6b6f9806e3f2b
jeffrey ren <jeffreyren@jeffreys-MacBook-Pro.local>
2024-05-12 23:18:12 +0900

    added scroll label

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,21 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     fontFamily:{
-      "serif":["Zen Tokyo Zoo"]
+      "serif":["Knewave"]
     },
     colors:{
       "sakura-pink": "#fcd1d7",
+      "paper": "#f6eee3",
+      "dark-blue": "#0c2e41",
+      "wood-brown": "#8e5f27",
+      "mustard": "#ca8a04",
+      "white": "#fff"
     },
-    extend: {},
+    extend: {
+      screens: {
+        "xs": "480px"
+      }
+    },
   },
   plugins: [],
 }
