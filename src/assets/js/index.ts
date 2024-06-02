892bf7672735797660c87d4faf24b28bf157e564
jeffrey ren <jeffreyren@jeffreys-MacBook-Pro.local>
2024-06-02 16:47:00 +0900

    refactored js page

--- src/assets/js/index.ts
+++ src/assets/js/index.ts
@@ -1,40 +1,6 @@
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-    
-gsap.registerPlugin(ScrollTrigger);
-
-const timeline = gsap.timeline({
-
-})
 
 
-//pin section top
-timeline.to("#HeroEl",{
-    scrollTrigger:{
-        trigger: ".scroll-wrapper",
-        pin: true,
-        start:"top",
-        end: "2000px"
-    },
 
-}).from("#ScrollCylinder",{
-    scrollTrigger:{
-       trigger: "#HeroEl",
-       scrub: 1,
-       start: "top",
-       end:"200"
-    },
-    scale:2,
-    x:"50vw",
 
-    rotate: "90deg"
-}).from(".scroll-content", {
-    scrollTrigger: {
-        trigger: "#HeroEl",
-        scrub: 3,
-        markers:true,
-         start: "500px",
-         end: "center 200px",
-    },
-    x: `-200%`,
-  });
+
+
